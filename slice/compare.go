fb61dfe2b38b2d145afdee2002276b934efd3ad4
marhaupe <marhaupe97@gmail.com>
2018-11-22 11:54:08 +0100

    EqualsUnordered is tested and works

--- slice/compare.go
+++ slice/compare.go
@@ -1,28 +1,59 @@
 package utils
 
 import (
+	"fmt"
 	"reflect"
 )
 
 // EqualsUnordered compares two slices without failing if they're not in the same order
-func EqualsUnordered(x []interface{}, y []interface{}) bool {
+func EqualsUnordered(x interface{}, y interface{}) bool {
 	if x == nil || y == nil {
 		return x == nil && y == nil
 	}
-	reflect.DeepEqual()
 
-	v1 := reflect.ValueOf(x)
-	v2 := reflect.ValueOf(y)
+	vx := reflect.ValueOf(x)
+	vy := reflect.ValueOf(y)
 
-	if v1.Type() != v2.Type() {
+	fmt.Println("X: ", fmt.Sprint(vx))
+	fmt.Println("Y: ", fmt.Sprint(vy))
+	// If it isn't an array or a slice, do the usual DeepEqual()
+	if vx.Kind() != reflect.Slice && vx.Kind() != reflect.Array {
+		return reflect.DeepEqual(x, y)
+	}
+
+	// Check if elements are the same type
+	if vx.Type().Elem() != vy.Type().Elem() {
 		return false
 	}
-	if len(x) != len(y) {
+
+	// Arrays / Slices are not the same if their length differs
+	if vx.Len() != vy.Len() {
 		return false
 	}
-	switch v1.Type() {
 
+	// From this point on, type and element type of array are the same.
+
+	if vx.Len() == 0 {
+		return true
 	}
 
+	xarr := make([]interface{}, vx.Len())
+	yarr := make([]interface{}, vy.Len())
+	for i := 0; i < vx.Len(); i++ {
+		xarr[i] = vx.Index(i).Interface()
+		yarr[i] = vy.Index(i).Interface()
 	}
+
+	for _, xel := range xarr {
+		contains := false
+		for _, yel := range yarr {
+			if reflect.DeepEqual(xel, yel) {
+				contains = true
+			}
+		}
+		if !contains {
+			return false
+		}
+	}
+	return true
 }
---
+++ slice/compare_test.go
@@ -0,0 +1,99 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestEqualsUnordered(t *testing.T) {
+	type args struct {
+		x interface{}
+		y interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Slice of Strings",
+			args: args{
+				x: []string{"Hallo", "Test", "Hier"},
+				y: []string{"Test", "Hallo", "Hier"},
+			},
+			want: true,
+		},
+		{
+			name: "Slice of Unequal Types",
+			args: args{
+				x: []int{1, 2, 3},
+				y: []string{"Test", "Hallo", "Hier"},
+			},
+			want: false,
+		},
+		{
+			name: "Slice of Structs",
+			args: args{
+				x: []struct {
+					ID   int
+					Text string
+				}{
+					{
+						ID:   1,
+						Text: "Eins",
+					}, {
+						ID:   2,
+						Text: "Zwei",
+					}, {
+						ID:   3,
+						Text: "Drei",
+					}},
+				y: []struct {
+					ID   int
+					Text string
+				}{
+					{
+						ID:   3,
+						Text: "Drei",
+					}, {
+						ID:   2,
+						Text: "Zwei",
+					}, {
+						ID:   1,
+						Text: "Eins",
+					}},
+			},
+			want: true,
+		},
+		{
+			name: "Slice of Numbers",
+			args: args{
+				x: []int{1, 2, 3},
+				y: []int{2, 1, 3},
+			},
+			want: true,
+		},
+		{
+			name: "Array of String",
+			args: args{
+				x: [...]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"},
+				y: [...]string{"J", "I", "H", "G", "F", "E", "D", "C", "B", "A"},
+			},
+			want: true,
+		},
+		{
+			name: "Array of Numbers",
+			args: args{
+				x: [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+				y: [...]int{2, 1, 3, 0, 4, 5, 7, 6, 8, 9},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := EqualsUnordered(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("EqualsUnordered() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

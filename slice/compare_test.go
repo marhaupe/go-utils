fe0628350adc1df81cd6381b7c9d17acba829aa4
marhaupe <marhaupe97@gmail.com>
2019-01-18 19:22:44 +0100

    added tests

--- slice/compare_test.go
+++ slice/compare_test.go
@@ -31,6 +31,40 @@ func TestEqualsUnordered(t *testing.T) {
 			want: false,
 		},
 		{
+			name: "Unequal Slice of Structs",
+			args: args{
+				x: []struct {
+					ID   int
+					Text string
+				}{
+					{
+						ID:   4,
+						Text: "Vier",
+					}, {
+						ID:   2,
+						Text: "Zwei",
+					}, {
+						ID:   3,
+						Text: "Drei",
+					}},
+				y: []struct {
+					ID   int
+					Text string
+				}{
+					{
+						ID:   3,
+						Text: "Drei",
+					}, {
+						ID:   2,
+						Text: "Zwei",
+					}, {
+						ID:   1,
+						Text: "Eins",
+					}},
+			},
+			want: false,
+		},
+		{
 			name: "Slice of Structs",
 			args: args{
 				x: []struct {
@@ -73,6 +107,14 @@ func TestEqualsUnordered(t *testing.T) {
 			want: true,
 		},
 		{
+			name: "Unequal Array of String",
+			args: args{
+				x: [...]string{"A", "B", "C"},
+				y: [...]string{"J", "I", "H"},
+			},
+			want: false,
+		},
+		{
 			name: "Array of String",
 			args: args{
 				x: [...]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"},
@@ -84,9 +126,57 @@ func TestEqualsUnordered(t *testing.T) {
 			name: "Array of Numbers",
 			args: args{
 				x: [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
-				y: [...]int{2, 1, 3, 0, 4, 5, 7, 6, 8, 9},
+				y: [...]int{0, 9, 8, 7, 6, 5, 4, 3, 2, 1},
 			},
 			want: true,
+		},
+		{
+			name: "Unequal array of numbers",
+			args: args{
+				x: [...]int{1, 2, 3},
+				y: [...]int{4, 5, 6},
+			},
+			want: false,
+		},
+		{
+			name: "Nil with something else",
+			args: args{
+				x: nil,
+				y: [...]int{2, 1, 3, 0, 4, 5, 7, 6, 8, 9},
+			},
+			want: false,
+		},
+		{
+			name: "String comparison",
+			args: args{
+				x: "X",
+				y: "Y",
+			},
+			want: false,
+		},
+		{
+			name: "Empty arrays, same type",
+			args: args{
+				x: []int{},
+				y: []int{},
+			},
+			want: true,
+		},
+		{
+			name: "Empty arrays, different type",
+			args: args{
+				x: []string{},
+				y: []int{},
+			},
+			want: false,
+		},
+		{
+			name: "Different arrays",
+			args: args{
+				x: [...]int{2, 1, 3, 0, 4, 5, 7, 6, 8},
+				y: [...]int{2, 1, 3, 0, 4, 5, 7, 6, 8, 9},
+			},
+			want: false,
 		},
 	}
 	for _, tt := range tests {
